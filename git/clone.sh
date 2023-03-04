512a4a75541a5e793fbc0e9d781ec9425651c263
root <chauncylee10@gmail.com>
2023-03-04 16:51:54 +0800

    first commit

--- git/clone.sh
+++ git/clone.sh
@@ -1,11 +1,12 @@
 git clone --recursive  https://github.com/grpc/grpc.git
 
+#  
 # git init
 
 git add .
 git commit -m "xxx"
 
 # git branch -M main
-# git remote add origin https://xxx@github.com/chauncylee10/myUtils.git
+# git remote add origin https://github.com/chauncylee10/myUtils.git
 
 git push -u origin main
